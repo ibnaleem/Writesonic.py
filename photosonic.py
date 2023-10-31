1885882ef1e1855bf3752151b79cb2b54807be80
ibnaleem <ibnaleem@outlook.com>
2023-10-31 15:29:51 +0000

    fix indentation

--- photosonic.py
+++ photosonic.py
@@ -5,26 +5,26 @@ class Photosonic:
     self.api_key = api_key
     self.name = name
 
-def generate_image(self, prompt:str, num_images:int=None, image_width:int=None, image_height:int=None) -> str:
-  
-  """Generate images with prompts
-  - Prompt Arg: Required
-  - API Key: Required"""
+  def generate_image(self, prompt:str, num_images:int=None, image_width:int=None, image_height:int=None) -> str:
+    
+    """Generate images with prompts
+    - Prompt Arg: Required
+    - API Key: Required"""
 
-  url = "https://api.writesonic.com/v1/business/photosonic/generate-image"
+    url = "https://api.writesonic.com/v1/business/photosonic/generate-image"
 
-  payload = {
-      "num_images": num_images,
-      "image_width": image_width,
-      "image_height": image_height,
-      "prompt": f"{prompt}"
-  }
-  headers = {
-      "accept": "application/json",
-      "content-type": "application/json",
-      "X-API-KEY": self.api_key
-  }
+    payload = {
+        "num_images": num_images,
+        "image_width": image_width,
+        "image_height": image_height,
+        "prompt": f"{prompt}"
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "X-API-KEY": self.api_key
+    }
 
-  response = requests.post(url, json=payload, headers=headers)
+    response = requests.post(url, json=payload, headers=headers)
 
-  return response.text
+    return response.text
