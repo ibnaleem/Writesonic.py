d660be358386abf16b6a03b5d5bac63b408206ef
ibnaleem <ibnaleem@outlook.com>
2023-10-31 15:29:35 +0000

    create conversation() method

--- chatsonic.py
+++ chatsonic.py
@@ -1,1 +1,39 @@
-import requests
+import requests, json
+
+class Chatsonic:
+  def __init__(self, api_key:str, name:str = "Chatsonic 1") -> None:
+    self.api_key = api_key
+    self.name = name
+    self.history_data = []
+
+  def conversation(self, input_text:str, language: str = "en", enable_google_results: bool = True, enable_memory: bool = True) -> str:
+
+    url = f"https://api.writesonic.com/v2/business/content/chatsonic?engine=premium&language={language}"
+
+    payload = {
+        "enable_google_results": str(enable_google_results).lower(),
+        "enable_memory": enable_memory,
+        "input_text": input_text
+    }
+    
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "X-API-KEY": self.api_key
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    if enable_memory:
+      # Parse the JSON response
+      response_data = response.json()
+
+      # Access the "message" field
+      message = response_data.get("message")
+
+      self.history_data.append({"is_sent": True, "message": input_text}, {"is_sent": False, "message": message})
+    
+    else:
+      pass
+
+    return response.text
