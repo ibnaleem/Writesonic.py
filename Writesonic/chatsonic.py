8d94184925cbb2bee0d4c84fe9c73867f20742ba
ibnaleem <ibnaleem@outlook.com>
2023-11-16 16:50:19 +0000

    api_key attr -> property

--- Writesonic/chatsonic.py
+++ Writesonic/chatsonic.py
@@ -1,39 +1,56 @@
 import requests
 
+
 class Chatsonic:
-  def __init__(self, api_key:str, name:str = "Chatsonic 1") -> None:
-    self.api_key = api_key
-    self.name = name
-    self.history_data = []
+    def __init__(self, api_key: str, name: str = "Chatsonic 1") -> None:
+        self.api_key = api_key
+        self.history_data = []
+        self.name = name
 
-  def conversation(self, input_text:str, language: str = "en", enable_google_results: bool = True, enable_memory: bool = True) -> str:
+    @property
+    def api_key(self) -> str:
+        return self._api_key
 
-    url = f"https://api.writesonic.com/v2/business/content/chatsonic?engine=premium&language={language}"
+    @api_key.setter
+    def api_key(self, value: str) -> None:
+        self._api_key = value
 
-    payload = {
-        "enable_google_results": str(enable_google_results).lower(),
-        "enable_memory": enable_memory,
-        "input_text": input_text
-    }
-    
-    headers = {
-        "accept": "application/json",
-        "content-type": "application/json",
-        "X-API-KEY": self.api_key
-    }
+    def conversation(
+        self,
+        input_text: str,
+        language: str = "en",
+        enable_google_results: bool = True,
+        enable_memory: bool = True,
+    ) -> str:
+        url = f"https://api.writesonic.com/v2/business/content/chatsonic?engine=premium&language={language}"
 
-    response = requests.post(url, json=payload, headers=headers)
+        payload = {
+            "enable_google_results": str(enable_google_results).lower(),
+            "enable_memory": enable_memory,
+            "input_text": input_text,
+        }
 
-    if enable_memory:
-      # Parse the JSON response
-      response_data = response.json()
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "X-API-KEY": self.api_key,
+        }
 
-      # Access the "message" field
-      message = response_data.get("message")
+        response = requests.post(url, json=payload, headers=headers)
 
-      self.history_data.append({"is_sent": True, "message": input_text}, {"is_sent": False, "message": message})
-    
-    else:
-      pass
+        if enable_memory:
+            # Parse the JSON response
+            response_data = response.json()
 
-    return response.text
+            # Access the "message" field
+            message = response_data.get("message")
+
+            self.history_data.append(
+                {"is_sent": True, "message": input_text},
+                {"is_sent": False, "message": message},
+            )
+
+        else:
+            pass
+
+        return response.text
--- Writesonic/photosonic.py
+++ Writesonic/photosonic.py
@@ -1,38 +1,52 @@
 import requests
 
+
 class Photosonic:
-  def __init__(self, api_key:str, name: str = "Photosonic 1") -> None:
-    self.api_key = api_key
-    self.name = name
+    def __init__(self, api_key: str, name: str = "Photosonic 1") -> None:
+        self.api_key = api_key
+        self.name = name
 
-  def generate_image(self, prompt:str, num_images:int=None, image_width:int=None, image_height:int=None) -> str:
-    
-    """Generate images with prompts
-    - Prompt Arg: Required"""
+    @property
+    def api_key(self) -> str:
+        return self._api_key
 
-    url = "https://api.writesonic.com/v1/business/photosonic/generate-image"
+    @api_key.setter
+    def api_key(self, value: str) -> None:
+        self._api_key = value
 
-    if not num_images:
-      num_images = 1
-    
-    if not image_height:
-      image_height = 500
+    def generate_image(
+        self,
+        prompt: str,
+        num_images: int = None,
+        image_width: int = None,
+        image_height: int = None,
+    ) -> str:
+        """Generate images with prompts
+        - Prompt Arg: Required"""
 
-    if not image_width:
-      image_width = 500    
-    
-    payload = {
-        "num_images": num_images,
-        "image_width": image_width,
-        "image_height": image_height,
-        "prompt": f"{prompt}"
-    }
-    headers = {
-        "accept": "application/json",
-        "content-type": "application/json",
-        "X-API-KEY": self.api_key
-    }
+        url = "https://api.writesonic.com/v1/business/photosonic/generate-image"
 
-    response = requests.post(url, json=payload, headers=headers)
+        if not num_images:
+            num_images = 1
 
-    return response.text
+        if not image_height:
+            image_height = 500
+
+        if not image_width:
+            image_width = 500
+
+        payload = {
+            "num_images": num_images,
+            "image_width": image_width,
+            "image_height": image_height,
+            "prompt": f"{prompt}",
+        }
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "X-API-KEY": self.api_key,
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+
+        return response.text
