bb5a0e2124b69de6134b5ecabee3cd9169c95586
ibnaleem <ibnaleem@outlook.com>
2023-11-17 13:23:40 +0000

    create article intro method

--- Writesonic/writesonic.py
+++ Writesonic/writesonic.py
@@ -1,34 +1,67 @@
 import requests
 
+
 class Writesonic:
-  def __init__(self, api_key:str, name:str = "Writesonic 1") -> None:
-    self.api_key = api_key
-    self.name = name
+    def __init__(self, api_key: str, name: str = "Writesonic 1") -> None:
+        self.api_key = api_key
+        self.name = name
 
-  @property
-  def api_key(self) -> str:
-    return self._api_key
+    @property
+    def api_key(self) -> str:
+        return self._api_key
 
-  @api_key.setter
-  def api_key(self, value: str) -> None:
-    self._api_key = value
+    @api_key.setter
+    def api_key(self, value: str) -> None:
+        self._api_key = value
 
-  def article_ideas(self, topic:str, primary_keyword:str = None, engine:str = "economy", language:str = "en", num_copies: int = 1):
-    url = f"https://api.writesonic.com/v2/business/content/blog-ideas?engine={engine}&language={language}&num_copies={num_copies}"
+    def article_ideas(
+        self,
+        topic: str,
+        primary_keyword: str = None,
+        engine: str = "economy",
+        language: str = "en",
+        num_copies: int = 1,
+    ):
+        url = f"https://api.writesonic.com/v2/business/content/blog-ideas?engine={engine}&language={language}&num_copies={num_copies}"
 
-    if not primary_keyword:
-      payload = {"topic": topic}
+        if not primary_keyword:
+            payload = {"topic": topic}
 
-    else:
-      payload = {"topic": topic, "primary_keyword": primary_keyword}
+        else:
+            payload = {"topic": topic, "primary_keyword": primary_keyword}
 
-    headers = {
-        "accept": "application/json",
-        "content-type": "application/json",
-        "X-API-KEY": self.api_key
-    }
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "X-API-KEY": self.api_key,
+        }
 
-    response = requests.post(url, json=payload, headers=headers)
+        response = requests.post(url, json=payload, headers=headers)
 
-    return response.text
-  
+        return response.text
+
+    def article_intro(
+        self,
+        topic: str,
+        primary_keyword: str = None,
+        engine: str = "economy",
+        language: str = "en",
+        num_copies: int = 1,
+    ):
+        url = f"https://api.writesonic.com/v2/business/content/blog-intros?engine={engine}&language={language}&num_copies={num_copies}"
+
+        if not primary_keyword:
+            payload = {"topic": topic}
+
+        else:
+            payload = {"topic": topic, "primary_keyword": primary_keyword}
+
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "X-API-KEY": self.api_key,
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+
+        return response.text
